ecf6c85f96cb8d81e4a10b86bbc13895f6efa9b4
fabrizio199 <78603022+fabrizio199@users.noreply.github.com>
2024-04-02 11:09:48 +0200

    Update Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
+    //test
     String cmd = "/usr/games/cowsay '" + input + "'";
     System.out.println(cmd);
     processBuilder.command("bash", "-c", cmd);
